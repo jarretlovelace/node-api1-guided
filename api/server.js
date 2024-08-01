049b6d18d66b01605a4d3d56a928023d4d4f17f3
Jarret Lovelace <jarretlovelace@icloud.com>
2024-08-01 13:45:37 -0600

    fixes

--- api/server.js
+++ api/server.js
@@ -1,16 +1,31 @@
 // IMPORTS AT THE TOP
-
+const express = require('express')
+const Dog = require('./dog-model')
 // INSTANCE OF EXPRESS APP
+const server = express()
 
 // GLOBAL MIDDLEWARE
-
+server.use(express.json())
 // ENDPOINTS
 
 // [GET]    /             (Hello World endpoint)
+server.get('/hello-world', (req, res) => {
+    res.status(200).json({ message: "hello, world "})
+})
+
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
+server.get('/api/dogs', async (req, res) => {
+    try {
+        const dogs = await Dog.findAll()
+console.log(dogs)
+    } catch(err) {
+        res.status(500).json({ message: `Something horrible: ${err.message}`})
+    }
+})
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
+module.exports = server 
